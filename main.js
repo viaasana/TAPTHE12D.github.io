711ed42dd0580b6f82e3f787fc7e8f4af3dc460e
viasana <107901948+viaasana@users.noreply.github.com>
2023-03-23 16:27:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var screenID = 0
 var foods = document.getElementsByClassName('food')
-var background_color = ['linear-gradient(rgb(9, 165, 163), rgb(255, 255, 255))', 'linear-gradient(rgb(120, 67, 14), rgb(255, 255, 255))', 'linear-gradient(rgb(120, 117, 14), rgb(255, 255, 255))', 'linear-gradient(rgb(26, 120, 14), rgb(255, 255, 255))', 'linear-gradient(rgb(30, 14, 120), rgb(255, 255, 255))']
+
+for (food =1; food<foods.length; food ++){
+    foods[food].style['display'] = 'none'
+}
 
 
+console.log(foods.length)
 function change_screen(i){
     screenID+=i
-    if (screenID>4){
+    if (screenID>3){
         screenID = 0
     }else if (screenID<0){
         screenID =3
     }
     for (food =0; food<foods.length; food ++){
-        foods[food].style['z-index'] = '0'
+        foods[food].style['display'] = 'none'
     }
-    foods[screenID].style['z-index'] = '1'
-    document.getElementById('background_image').style['background-image'] = background_color[screenID]
+    foods[screenID].style['display'] = 'unset'
 }
